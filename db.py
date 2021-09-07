037692c408c5bde375680dc0f1cee8aadba814ee
weweEQWE <85250561+weweEQWE@users.noreply.github.com>
2021-09-07 23:29:22 +0600

    add connectors

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
-from psycopg2 import connect, Error
+from psycopg2 import connect as postgresql_connect, Error as postgresql_Error
+from sqlite3 import connect as sqlite_connect, Error as sqlite_Error
+from mysql.connector import connect as mysql_connect, Error as mysql_Error
 
 
 class DataBaseError(Exception):
@@ -6,17 +8,28 @@ class DataBaseError(Exception):
 
 
 class DataBase:
-    def __init__(self, config: dict):
+    def __init__(self, config: dict, db_type):
         self.config = config
-
+        self.db_type = db_type
+    
     def __enter__(self):
+        if db_type == 'MySQL':
+            connect = mysql_connect
+            Error = mysql_Error
+        elif db_type == 'PostgresSQL':
+            connect = postgresql_connect
+            Error = postgresql_Error
+        elif db_type == 'SQLite':
+            connect = sqlite_connect
+            Error = sqlite_Error
+
         try:
             self.conn = connect(**self.config)
             self.cursor = self.conn.cursor()
             return self.cursor
         except Error as err:
-            self.conn.rollback()
             raise DataBaseError(err)
+            self.conn.rollback()
 
     def __exit__(self, exc_type, exc_value, exc_trace):
         self.conn.commit()
